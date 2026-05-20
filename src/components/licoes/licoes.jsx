546ed75ad1933169ef0f6a1a8cfb7a2fda58535a
Isabela Santiago <isadelga2009@gmail.com>
2026-05-20 16:51:39 -0300

    feat: incrementando um botão para adicionar lições

--- src/components/licoes/licoes.jsx
+++ src/components/licoes/licoes.jsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import { Avatar } from 'primereact/avatar';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
-import profileImage from '../../assets/Logo_sem_titulo.png';
+import './licoes.css';
+import plusbtn from '../../assets/plus.svg';
 
 export default function Licoes() {
   const footer = (
     <div className="flex flex-column align-items-center">
-      
     </div>
   );
 
   return (
     <div className="sobre flex justify-content-center align-items-center">
-      <Card
-        title="Lições"
-        footer={footer}
-      >
-        
-      </Card>
+        <Card title="Lições" footer={footer}>
+            <Button className="btn-add" rounded>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white    " stroke-width="4" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+            </Button>
+        </Card>
     </div>
   );
 }
