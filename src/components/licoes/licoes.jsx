4e1b56b9e6a96dd27483a8a71a1aa3b7265ac8a4
Isabela Santiago <isadelga2009@gmail.com>
2026-05-21 19:49:31 -0300

    feat: adicionando funcionalidades

--- src/components/licoes/licoes.jsx
+++ src/components/licoes/licoes.jsx
@@ -3,6 +3,19 @@ import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import './licoes.css';
 import plusbtn from '../../assets/plus.svg';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 
 export default function Licoes() {
   const footer = (
@@ -10,13 +23,47 @@ export default function Licoes() {
     </div>
   );
 
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="sobre flex justify-content-center align-items-center">
-        <Card title="Lições" footer={footer}>
-            <Button className="btn-add" rounded>
+        <Card title="Conteúdo das lições" footer={footer}>
+            <Button className="btn-add" rounded onClick={openModal}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white    " stroke-width="4" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
             </Button>
+            <Modal
+              isOpen={modalIsOpen}
+              style={customStyles}
+            >
+              <div className="modal">
+                  <h1>Lição</h1>
+                  <form className="form-modal">
+                    <label htmlFor="questao">Escreva a questão: </label><br />
+                    <input type="text" /><br /><br />
+
+                    <label htmlFor="alternativaA">Insira a alternativa A:</label><br />
+                    <input type="text" /><br /><br />
+
+                    <label htmlFor="alternativaB">Insira a alternativa B:</label><br />
+                    <input type="text" /><br /><br />
+
+                    <label htmlFor="alternativaC">Insira a alternativa C:</label><br />
+                    <input type="text" /><br /><br />
+                  </form>
+              <button onClick={closeModal}>Fechar</button> 
+              </div>
+            </Modal>
         </Card>
     </div>
   );
 }
+
